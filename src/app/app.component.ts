c9d4e904b1feba4aedb1fef46b792d4a9a04b99d
unknown <shailendra.prajapati@Shailendra-P.in.corp.tavant.com>
2017-07-10 19:07:25 +0530

    ng-2 setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  formData:IRegisterForm;
+
+  constructor(){
+
+    this.formData = {
+      "name":"Shailendra",
+      "email":"sprajjobid@gmail.com",
+      "userName":"Shailendra0102",
+      "password":"Password0102",
+      "cnfrmPassword":"Password0102"
+    };
+
+  };
+
+  register(value:any){
+    console.log("this.for===>"+value.name);
+  }
+  
+}
+
+interface IRegisterForm {
+  name:string;
+  email:string;
+  userName:string;
+  password:string;
+  cnfrmPassword:string;
 }
